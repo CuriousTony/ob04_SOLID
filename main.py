cfc749057547fd1e9a3f72e8700d36aec2addd52
CuriousTony <aszdorovtsev@yandex.ru>
2024-09-25 17:55:13 +0300

    Changed all the logics to suite the targets concept.

--- main.py
+++ main.py
@@ -2,66 +2,71 @@ from abc import ABC, abstractmethod
 from random import randint
 
 
-class Creature(ABC):
-    def __init__(self, name, health):
-        self.name = name
-        self.health = health
-
-    @abstractmethod
-    def get_damage(self, damage_in):
-        self.damage_in = damage_in
-        pass
-
+class Weapon(ABC):
     @abstractmethod
     def attack(self):
         pass
 
 
-class Monster(Creature):
-    def __init__(self, name, health):
-        super().__init__(name, health)
-
-    def get_damage(self, damage_in):
-        if self.health > 1:
-            self.health -= damage_in
-            print(f"Чудище {self.name.title()} получило {damage_in} урона.\n"
-                  f"Оставшиеся очки здоровья - {self.health}.\n")
-        elif self.health <= 0:
-            print(f"{self.name.title()} падает замертво."
-                  f"Смертельный удар нанес ему {self.damage_in} урона.")
-
-    def attack(self, target):
-        damage_out = randint(10, 40)
-        print(f"Чудище {self.name.title()} наносит размашистый удар!")
-        target.get_damage(damage_out)
+class Sword(Weapon):
+    def attack(self):
+        score = randint(25, 50)
+        print(f"Удар мечом наносит страшную рану, нанося противнику {score} очков урона!")
+        return score
 
 
-class Knight(Creature):
-    def __init__(self, name, health):
-        super().__init__(name, health)
+class Bow(Weapon):
+    def attack(self):
+        score = randint(15, 30)
+        print(f"Выстрел из лука причиняет противнику мучения, нанося {score} урона!")
+        return score
 
-    def get_damage(self, damage_in):
-        if self.health >= 1:
-            self.health -= damage_in
-            print(f"Рыцарь {self.name.title()} получил {damage_in} урона.\n"
-                  f"Оставшиеся очки здоровья - {self.health}.\n")
-        elif self.health <= 0:
-            print(f"Рыцарь {self.name.title()} падает замертво."
-                  f"Смертельный удар нанес ему {self.damage_in} урона.")
+
+class Bazooka(Weapon):
+    def attack(self):
+        score = randint(60,100)
+        print(f"Выстрел из базуки оглушает всех присутствующих, нанося цели {score} урона.")
+        return score
+
+
+class Fighter:
+    def __init__(self, name, weapon):
+        self.name = name
+        self.weapon = weapon
 
     def attack(self, target):
-        damage_out = randint(10, 30)
-        print(f"Рыцарь {self.name.title()} кровожадно громит врага.")
-        target.get_damage(damage_out)
+        print(f"{self.name.title()} атакует!")
+        damage_done = self.weapon.attack()
+        target.get_damage(damage_done)
+
+    def change_weapon(self, new_weapon):
+        print(f"{self.name.title()} сменил оружие!")
+        self.weapon = new_weapon
 
 
-knight = Knight('Титус', 100)
-monster = Monster('Мргл-мргл', 120)
+class Monster:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
 
-knight.attack(monster)
-monster.attack(knight)
+    def get_damage(self, damage):
+        if self.hp > 0:
+            self.hp -= damage
+            print(f"Остаток очков здоровья {self.name.title()} {self.hp}hp.")
+        if self.hp <= 0:
+            print(f"Монстр по имени {self.name.title()} не выдержал такого "
+                  f"напора и отъехал в мир иной.")
 
-# class Weapon(ABC):
-#     @abstractmethod
-#     def hit_target(self):
-#         pass
+
+sword = Sword()
+bow = Bow()
+bazooka = Bazooka()
+
+
+monster = Monster('sqeegl', 100)
+swordsman = Fighter('titus', sword)
+swordsman.attack(monster)
+swordsman.change_weapon(bow)
+swordsman.attack(monster)
+swordsman.change_weapon(bazooka)
+swordsman.attack(monster)
